ff64b1b11b1d3286200c722a0c7f9c1c18560fdd
hjibarram <hjibarram@gmail.com>
2024-03-01 12:56:22 -0600

    update

--- MapLines/tools/models.py
+++ MapLines/tools/models.py
@@ -2,68 +2,46 @@
 import MapLines.tools.tools as tol
 import numpy as np
 
-'''
-def line_model_hb(theta, x=0, xo1=0, xo2=0, xo3=0 ,ret_com=False):
-    A1,A3,fac,dv1,dv2,fwhm1,fwhm2,A7,dv3=theta
+def emission_line_model(x, xo=0, A=1.0, dv=[0.0], fwhm=[200.0], fac=[0.7]):
     ct=299792.458
-    
-    sigma1=fwhm1/ct*xo1/(2.0*np.sqrt(2.0*np.log(2.0)))
-    #sigmaO=fwhmO/ct*xo1/(2.0*np.sqrt(2.0*np.log(2.0)))
-    xo_b=xo1*(1.0+dv1/ct)
-    xo_r=xo1*(1.0+dv2/ct)
-    Gb=tol.gauss_M(x,sigma=sigma1,xo=xo_b,A1=A1)
-    A2=A1/fac
-    Gr=tol.gauss_M(x,sigma=sigma1,xo=xo_r,A1=A2)
-    
-    sigma2=fwhm1/ct*xo2/(2.0*np.sqrt(2.0*np.log(2.0)))
-    xo_b=xo2*(1.0+dv1/ct)
-    xo_r=xo2*(1.0+dv2/ct)
-    hGb=tol.gauss_M(x,sigma=sigma2,xo=xo_b,A1=A3)
-    A4=A3/fac
-    hGr=tol.gauss_M(x,sigma=sigma2,xo=xo_r,A1=A4)
-    
-    sigma3=fwhm1/ct*xo3/(2.0*np.sqrt(2.0*np.log(2.0)))
-    xo_b=xo3*(1.0+dv1/ct)
-    xo_r=xo3*(1.0+dv2/ct)
-    A5=A1/3.0
-    A6=A2/3.0
-    nGb=tol.gauss_M(x,sigma=sigma3,xo=xo_b,A1=A5)
-    nGr=tol.gauss_M(x,sigma=sigma3,xo=xo_r,A1=A6)
-
-    sigma4=fwhm2/ct*xo2/(2.0*np.sqrt(2.0*np.log(2.0)))
-    xo=xo2*(1.0+dv3/ct)
-    hGbr=tol.gauss_M(x,sigma=sigma4,xo=xo,A1=A7)
-    
-    lin=Gb+Gr+hGb+hGr+nGb+nGr+hGbr
-    if ret_com:
-        return lin,Gb,Gr,hGb,hGr,nGb,nGr,hGbr
-    else:
-        return lin
-'''        
+    model_out=[]
+    for i in range(len(dv)):
+        sigma=fwhm[i]/ct*xo/(2.0*np.sqrt(2.0*np.log(2.0)))
+        xm=xo*(1.0+dv[i]/ct)
+        if i > 0:
+            A=A/fac[i-1]
+        model=tol.gauss_M(x,sigma=sigma,xo=xm,A1=A1)
+        model_out.extend([model])
+    return model_out
+        
 
 def line_model(theta, x=0, xo1=0, xo2=0, xo3=0 ,ret_com=False, lfac12=2.93):
     A1,A3,fac,dv1,dv2,fwhm1,fwhm2,A7,dv3=theta
     ct=299792.458
-    sigma1=fwhm1/ct*xo1/(2.0*np.sqrt(2.0*np.log(2.0)))
-    xo_b=xo1*(1.0+dv1/ct)
-    xo_r=xo1*(1.0+dv2/ct)
-    Gb=tol.gauss_M(x,sigma=sigma1,xo=xo_b,A1=A1)
-    A2=A1/fac
-    Gr=tol.gauss_M(x,sigma=sigma1,xo=xo_r,A1=A2)
     
+    Gb,Gr=emission_line_model(x, xo=xo1, A=A1, dv=[dv1,dv2], fwhm=[fwhm1,fwhm1], fac=[fac])
+
+    #sigma1=fwhm1/ct*xo1/(2.0*np.sqrt(2.0*np.log(2.0)))
+    #xo_b=xo1*(1.0+dv1/ct)
+    #Gb=tol.gauss_M(x,sigma=sigma1,xo=xo_b,A1=A1)
+    #xo_r=xo1*(1.0+dv2/ct)
+    #A2=A1/fac
+    #Gr=tol.gauss_M(x,sigma=sigma1,xo=xo_r,A1=A2)
+    
+
     sigma2=fwhm1/ct*xo2/(2.0*np.sqrt(2.0*np.log(2.0)))
     xo_b=xo2*(1.0+dv1/ct)
-    xo_r=xo2*(1.0+dv2/ct)
     hGb=tol.gauss_M(x,sigma=sigma2,xo=xo_b,A1=A3)
+    xo_r=xo2*(1.0+dv2/ct)
     A4=A3/fac
     hGr=tol.gauss_M(x,sigma=sigma2,xo=xo_r,A1=A4)
     
     sigma3=fwhm1/ct*xo3/(2.0*np.sqrt(2.0*np.log(2.0)))
     xo_b=xo3*(1.0+dv1/ct)
-    xo_r=xo3*(1.0+dv2/ct)
     A5=A1/lfac12
-    A6=A2/lfac12
     nGb=tol.gauss_M(x,sigma=sigma3,xo=xo_b,A1=A5)
+    xo_r=xo3*(1.0+dv2/ct)
+    A6=A2/lfac12
     nGr=tol.gauss_M(x,sigma=sigma3,xo=xo_r,A1=A6)
     
     sigma4=fwhm2/ct*xo2/(2.0*np.sqrt(2.0*np.log(2.0)))
