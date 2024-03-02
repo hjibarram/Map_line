aacc6e1609e449e865a5dcbcd3373991967d2b80
hjibarram <hjibarram@gmail.com>
2024-03-02 16:24:53 -0600

    Update tools.py

--- MapLines/tools/tools.py
+++ MapLines/tools/tools.py
@@ -29,8 +29,8 @@ def conv(xt,ke=2.5):
 def gauss_K(x,sigma=1.0,xo=0.0,A1=1.0,alp=0):
     dt=alp/np.sqrt(np.abs(1+alp**2))
     xot=xo-sigma*dt*np.sqrt(2/np.pi)
-    #omega=np.sqrt(sigma**2.0/(1-2*dt**2/np.pi))
-    omega=sigma
+    omega=np.sqrt(sigma**2.0/(1-2*dt**2/np.pi))
+    #omega=sigma
     t=(x-xot)/omega
     Phi=(1+errf(alp*t/np.sqrt(2.0)))
     Ghi=np.exp(-0.5*t**2.0)
