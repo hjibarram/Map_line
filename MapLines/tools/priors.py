160e91540c1b84af10612464dea3e88dd9ea4109
hjibarram <hjibarram@gmail.com>
2024-04-17 11:27:06 -0600

    Update priors.py

--- MapLines/tools/priors.py
+++ MapLines/tools/priors.py
@@ -46,15 +46,16 @@ def lnprior_gauss_Lin(theta, valsp, At=0.05,dv1t=200,sim=False, single=False, sk
         if skew:
             A1,A3,dv1,fwhm1,fwhm2,A7,dv3,alp1,alpb=theta
             #if ((A1 >= 0) and (A1 <=1.8)) and ((A3 >= 0) and (A3 <=0.8)) and ((A7 >= Am1) and (A7 < Am2)) and ((fwhm1 >= 100.0) and (fwhm1 <= 900.0)) and ((fwhm2 >= 700.0) and (fwhm2 <= 10500.0)) and ((dv1 >= dv1i) and (dv1 <= dv1s)) and ((dv3 >= -1000) and (dv3 <= 1000)) and ((alp1 >= -10) and (alp1 <= 10)) and ((alpb >= -10) and (alpb <= 10)): 
-            if ((A1 >= valsp['a1i']) and (A1 <= valsp['a1s'])) and ((A3 >= valsp['a3i']) and (A3 <= valsp['a3s'])) and ((A7 >= Am1) and (A7 < Am2)) and ((fwhm1 >= valsp['fwhm1i']) and (fwhm1 <= valsp['fwhm1s'])) and ((fwhm2 >= valsp['fwhm2i']) and (fwhm2 <= valsp['fwhm2s'])) and ((dv1 >= valsp['dv1i']) and (dv1 <= valsp['dv1s'])) and ((dv3 >= valsp['dv3i']) and (dv3 <= valsp['dv3s'])) and ((alp1 >= -10) and (alp1 <= 10)) and ((alpb >= -10) and (alpb <= 10)):     
+            #if ((A1 >= valsp['a1i']) and (A1 <= valsp['a1s'])) and ((A3 >= valsp['a3i']) and (A3 <= valsp['a3s'])) and ((A7 >= Am1) and (A7 < Am2)) and ((fwhm1 >= valsp['fwhm1i']) and (fwhm1 <= valsp['fwhm1s'])) and ((fwhm2 >= valsp['fwhm2i']) and (fwhm2 <= valsp['fwhm2s'])) and ((dv1 >= valsp['dv1i']) and (dv1 <= valsp['dv1s'])) and ((dv3 >= valsp['dv3i']) and (dv3 <= valsp['dv3s'])) and ((alp1 >= -10) and (alp1 <= 10)) and ((alpb >= -10) and (alpb <= 10)):     
                 return 0.0
             else:
                 return -np.inf    
         else:
             if broad:
                 A1,A3,dv1,fwhm1,fwhm2,A7,dv3=theta
-                #if ((A1 >= 0) and (A1 <=1.8)) and ((A3 >= 0) and (A3 <=0.8)) and ((A7 >= Am1) and (A7 < Am2)) and ((fwhm1 >= 100.0) and (fwhm1 <= 800.0)) and ((fwhm2 >= 700.0) and (fwhm2 <= 10500.0)) and ((dv1 >= dv1i) and (dv1 <= dv1s)) and ((dv3 >= -1000) and (dv3 <= 1000)): 
-                if ((A1 >= valsp['a1i']) and (A1 <= valsp['a1s'])) and ((A3 >= valsp['a3i']) and (A3 <= valsp['a3s'])) and ((A7 >= Am1) and (A7 < Am2)) and ((fwhm1 >= valsp['fwhm1i']) and (fwhm1 <= valsp['fwhm1s'])) and ((fwhm2 >= valsp['fwhm2i']) and (fwhm2 <= valsp['fwhm2s'])) and ((dv1 >= valsp['dv1i']) and (dv1 <= valsp['dv1s'])) and ((dv3 >= valsp['dv3i']) and (dv3 <= valsp['dv3s'])): 
+                print(theta)
+                if ((A1 >= 0) and (A1 <=1.8)) and ((A3 >= 0) and (A3 <=0.8)) and ((A7 >= Am1) and (A7 < Am2)) and ((fwhm1 >= 100.0) and (fwhm1 <= 800.0)) and ((fwhm2 >= 700.0) and (fwhm2 <= 10500.0)) and ((dv1 >= dv1i) and (dv1 <= dv1s)) and ((dv3 >= -1000) and (dv3 <= 1000)): 
+                #if ((A1 >= valsp['a1i']) and (A1 <= valsp['a1s'])) and ((A3 >= valsp['a3i']) and (A3 <= valsp['a3s'])) and ((A7 >= Am1) and (A7 < Am2)) and ((fwhm1 >= valsp['fwhm1i']) and (fwhm1 <= valsp['fwhm1s'])) and ((fwhm2 >= valsp['fwhm2i']) and (fwhm2 <= valsp['fwhm2s'])) and ((dv1 >= valsp['dv1i']) and (dv1 <= valsp['dv1s'])) and ((dv3 >= valsp['dv3i']) and (dv3 <= valsp['dv3s'])): 
                     return 0.0
                 else:
                     return -np.inf
