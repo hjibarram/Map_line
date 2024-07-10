9edc751681a4f34ef106c74def610ea9e9452cf8
hjibarram <hjibarram@gmail.com>
2024-07-10 09:45:13 -0600

    Update tools.py

--- MapLines/tools/tools.py
+++ MapLines/tools/tools.py
@@ -85,11 +85,11 @@ def read_config_file(file):
         print('Config File not found')
         return None
 
-def get_fluxline(file,path='',in1=3,ind2=7,lo=6564.632):
+def get_fluxline(file,path='',ind1=3,ind2=7,lo=6564.632):
     file0=path+'/'+file
     [pdl_cube0, hdr0]=fits.getdata(file0, 0, header=True)
-    Amp=pdl_cube0[in1,:,:]
-    fwhm=pdl_cube0[in2,:,:]
+    Amp=pdl_cube0[ind1,:,:]
+    fwhm=pdl_cube0[ind2,:,:]
     sigma=fwhm/ct*xo1/(2.0*np.sqrt(2.0*np.log(2.0)))
     flux=np.sqrt(2.0*np.pi)*sigma*Amp
     return flux,sigma
