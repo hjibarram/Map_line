1e93aa439870c856b822c631e716ce7d60c515f1
hjibarram <hjibarram@gmail.com>
2025-04-05 15:32:50 -0600

    Update line_fit.py

--- MapLines/tools/line_fit.py
+++ MapLines/tools/line_fit.py
@@ -793,7 +793,8 @@ def line_fit(file1,file2,file3,file_out,file_out2,name_out2,z=0.05536,j_t=0,i_t=
                     else:
                         f_parm=theta_max
                     models=mod.line_model(theta_max, waves0, fac0, facN0, names0, n_lines, vals, ret_com=False, skew=skew, outflow=outflow)
-                print(models)
+                test=mod.emission_line_model(x, xo=[6734], A=[2], dv=[12] ,fwhm=[150])
+                print(test)
                 model=models[0]
                 modsI=models[1:]
                 model_all[:,i,j]=model
