9cab61eb7a9c0aae66559b865c55a15b9ca1e99d
hjibarram <hjibarram@gmail.com>
2025-06-10 16:14:07 +0200

    Update plot_tools.py

--- MapLines/tools/plot_tools.py
+++ MapLines/tools/plot_tools.py
@@ -39,9 +39,10 @@ def plot_bpt_map(file,name='',alpha=1,orientation=None,hd=0,max_typ=5,location=N
 
     ratio1=np.log10(fluxOIII/fluxHb)
     ratio2=np.log10(fluxNII/fluxHa)
-    print(ratio2)
+    print(ewHa)
     bounds = np.arange(0, max_typ + 1) + 0.5  # Para centrar los ticks
     map_bpt=tools.bpt(ewHa,ratio2,ratio1,ret=ret,agn=agn,sf=sf,inte=inte,comp=comp)
+    print(map_bpt)
 
     type_p=r'log($[OIII]H\beta$)~vs~log($[NII]H\alpha$)'
     type_n=r'log($[OIII]/H\beta$) vs log($[NII]/H\alpha$)'
