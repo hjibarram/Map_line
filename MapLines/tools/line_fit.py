459a0d86aa2e255023a60ce28983797cb06e2715
hjibarram <hjibarram@gmail.com>
2025-08-05 19:44:23 -0600

    updates

--- MapLines/tools/line_fit.py
+++ MapLines/tools/line_fit.py
@@ -549,7 +549,7 @@ def line_fit(file1,file2,file3,file_out,file_out2,name_out2,dir_out='',colors=['
         model_Outflow=np.zeros([len(nw),nx,ny])
     if powlaw:
         model_Powerlaw=np.zeros([len(nw),nx,ny])
-    valsp,n_lines,wavec1,wavec2=tol.get_priorsvalues(config_lines)    
+    valsp,n_lines,wavec1,wavec2,Inpvalues,Infvalues,Supvalues=tol.get_priorsvalues(config_lines)    
     '''
     data_lines=tol.read_config_file(config_lines)
     if data_lines:
