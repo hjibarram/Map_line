c2096a9a0be6eb16894f9ce1b6d4c4662542f757
hjibarram <hjibarram@gmail.com>
2025-08-26 14:19:18 -0600

    Update plot_tools.py

--- MapLines/tools/plot_tools.py
+++ MapLines/tools/plot_tools.py
@@ -246,7 +246,7 @@ def plot_bpt_map(file,name='',alpha=1,orientation=None,hd=0,ewsing=1,max_typ=5,l
         plt.show()
 
 
-def plot_single_map(file,valmax,valmin,name='',scale=0,sb=False,fwcs=False,logs=False,zerofil=False,valz=None,scalef=1.0,basefigname='Ha_vel_map_NAME',sumc=False,path='',hd=0,indx=0,indx2=None,tit='',lab='',facp=0.8,cont=False,alpha=1,orientation=None,location=None,savef=False,fig_path=''):
+def plot_single_map(file,valmax,valmin,name='',scale=0,sb=False,fwcs=False,logs=False,zerofil=False,valz=None,scalef=1.0,basefigname='Ha_vel_map_NAME',sumc=False,path='',hd=0,indx=0,indx2=None,tit='',lab='',facpx=0.8,facpy=0.8,cont=False,alpha=1,orientation=None,location=None,savef=False,fig_path=''):
 
     [data,hdr]=fits.getdata(path+'/'+file, hd, header=True)
     try:
@@ -285,7 +285,7 @@ def plot_single_map(file,valmax,valmin,name='',scale=0,sb=False,fwcs=False,logs=
     if logs:
         map_val=np.log10(map_val)
     
-    plt.rcParams['figure.figsize'] = [6.5*facp, 7.6*facp]
+    plt.rcParams['figure.figsize'] = [6.5*facpx, 7.6*facpy]
     if fwcs:
         wcs = WCS(hdr).celestial
         plt.subplot(projection=wcs)
