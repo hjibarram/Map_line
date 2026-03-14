71ea4830300415fbf94718b911e823333b4a13b3
hjibarram <hjibarram@gmail.com>
2026-03-14 16:24:46 -0600

    Update line_fit.py

--- MapLines/tools/line_fit.py
+++ MapLines/tools/line_fit.py
@@ -416,11 +416,11 @@ def line_fit_single(file1,file_out,file_out2,name_out2,dir_out='',
     fits_new_cols=[]
     print(model_param.shape)
     print(model_param)
-    print(model_param[0:1].shape)
+    print(model_param[0:1].ndim)
     print(model_param[0])    
     if len(keys_param) > 0:
         for i in range(len(keys_param)):
-            fits_new_cols.extend([fits.Column(name=keys_param[i], format=tol.numpy_to_tform(model_param[i]), array=model_param[i])])
+            fits_new_cols.extend([fits.Column(name=keys_param[i], format=tol.numpy_to_tform(model_param[i:i+1]), array=model_param[i:i+1])])
     coldefs = fits.ColDefs(fits_new_cols)
     h2 = fits.BinTableHDU.from_columns(coldefs)
     h_y=h2.header
