d64a11d75078279b8f1df4495cdee537114c77ec
hjibarram <hjibarram@gmail.com>
2026-06-26 12:06:19 -0600

    Update tools.py

--- MapLines/tools/tools.py
+++ MapLines/tools/tools.py
@@ -2448,18 +2448,18 @@ def get_mapmodel(name,path_map='./',path_out='./',basename='NAME-2iter_param_V2_
         mapT[np.where(amp_val == AmpT)]=np.nan
     if pow_cr:
         #Use the power line continum to define the usefull spaxels
-        #try:
-        if True:
+        try:
             # We define the lowest power line continuum value as the one for which we set the map to NaN, to avoid problems with the logarithm and the normalization. This is because in some cases there are very low continuum values that produce very high flux/continuum ratios, which are not realistic.
             indx_pow=get_map_param(hdr,keymatch='Amp_powerlow')
             amp_pow=pdl_cube[indx_pow,:,:]
             mintc=np.nanmin(amp_pow)
-            indx2=np.where(np.isfinite(mapT) == False)
+            indx2=np.where(np.isfinite(amp_pow) == False)
             mapT[np.where(amp_pow==mintc)]=np.nan
             mapT[indx2]=np.nan
-            print('Hola')
-        #except:
-        #    pass
+            if verbose:
+            print('Power-law used')
+        except:
+            pass
     #mapT=np.log10(mapT)
     mapT[np.where(np.isfinite(mapT) == False)]=-2
     #map[0:4,0:ny]=0
