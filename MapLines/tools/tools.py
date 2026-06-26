935d1be0981966d29b1d4abc3ddb81e7d4947641
hjibarram <hjibarram@gmail.com>
2026-06-26 12:03:34 -0600

    Update tools.py

--- MapLines/tools/tools.py
+++ MapLines/tools/tools.py
@@ -2457,6 +2457,7 @@ def get_mapmodel(name,path_map='./',path_out='./',basename='NAME-2iter_param_V2_
             indx2=np.where(np.isfinite(mapT) == False)
             mapT[np.where(amp_pow==mintc)]=np.nan
             mapT[indx2]=np.nan
+            print('Hola')
         #except:
         #    pass
     #mapT=np.log10(mapT)
